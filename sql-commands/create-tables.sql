b925149874312e8e9865a72e4ef9e2c910937d5e
Brian Hoblin <brian.hoblin@gmail.com>
2023-03-05 15:07:17 -0500

    Final create table update, I hope

--- sql-commands/create-tables.sql
+++ sql-commands/create-tables.sql
@@ -1,28 +1,42 @@
+alter table country drop constraint product_id_fk1;
+alter table product drop constraint country_id_fk;
+alter table product drop constraint element_code_fk;
+alter table element_data drop constraint product_id_fk2;
+
+commit;
+
+drop table country;
+drop table product;
+drop table element_data;
+drop table user_info;
+drop table user_query;
+drop table User_Info;
+drop table User_Query;
+drop table population;
+
+commit;
+
 create table Country(
     country_id NUMBER,
     country_name VARCHAR2(100),
+    product_id NUMBER,
     CONSTRAINT country_pk PRIMARY KEY (country_id)
 );
 
-commit;
-
 create table Product(
     product_id NUMBER,
+    country_id NUMBER,
+    element_code NUMBER,
     product_name VARCHAR2(100),
-    CID NOT NULL REFERENCES Country (country_id),
     UName VARCHAR2(100),
     CONSTRAINT product_pk PRIMARY KEY (product_id)
 );
-
-commit;
 
 create table User_Info(
     user_name VARCHAR2(100),
     pass_word VARCHAR2(100),
     CONSTRAINT user_pk PRIMARY KEY (user_name)
 );
-
-commit;
 
 create table User_Query(
     user_name NOT NULL REFERENCES User_Info (user_name),
@@ -31,18 +45,15 @@ create table User_Query(
     CONSTRAINT user_query_pk PRIMARY KEY (user_name, query_name)
 );
 
-commit;
-
 create table Element_Data(
     element_code NUMBER,
+    product_id NUMBER,
     element_name VARCHAR2(100),
     element_unit VARCHAR2(20),
     element_value DECIMAL(31,2),
     element_yr NUMBER,
     CONSTRAINT element_pk PRIMARY KEY (element_code)
 );
-
-commit;
 
 create table Population(
     pop_yr NUMBER,
@@ -53,7 +64,29 @@ create table Population(
     pop_country_id NUMBER,
     pop_product_id NUMBER,
     CONSTRAINT pop_of_country_fk1 FOREIGN KEY (pop_country_id) REFERENCES Country (country_id),
-    CONSTRAINT pop_of_country_fk2 FOREIGN KEY (pop_country_id) REFERENCES Product (product_id)
+    CONSTRAINT pop_of_country_fk2 FOREIGN KEY (pop_product_id) REFERENCES Product (product_id)
 );
 
+commit;
+
+ALTER TABLE Country ADD CONSTRAINT product_id_fk1 
+    FOREIGN KEY (product_id) 
+    REFERENCES Product (product_id) 
+    INITIALLY DEFERRED DEFERRABLE;
+    
+ALTER TABLE Product ADD CONSTRAINT country_id_fk
+    FOREIGN KEY (country_id)
+    REFERENCES Country (country_id)
+    INITIALLY DEFERRED DEFERRABLE;
+
+ALTER TABLE Product ADD CONSTRAINT element_code_fk
+    FOREIGN KEY (element_code)
+    REFERENCES Element_Data (element_code)
+    INITIALLY DEFERRED DEFERRABLE;
+    
+ALTER TABLE Element_Data ADD CONSTRAINT product_id_fk2 
+    FOREIGN KEY (product_id) 
+    REFERENCES Product (product_id) 
+    INITIALLY DEFERRED DEFERRABLE;
+    
 commit;
