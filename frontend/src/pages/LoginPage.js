ae14618e894978b723c6b127b188538d02326c0e
mralderman <aldermanza@gmail.com>
2023-03-17 14:03:47 -0400

    added places for login info and link to create account

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,8 +1,24 @@
+import {useState} from 'react';
+import {Link} from 'react-router-dom';
 const LoginPage = () => {
+    const [username, setUserName] = useState('');
+    const [Password, setPassWord] = useState('');
+    const [error, setError] = useState('');
     return (
         <>
         <h1>Login Page Stub</h1>
-        <p>This is the login page. Ideally, only either the My Profile or Login page will be present at a time. Meaning, once a user logs in they will see the My Profile option instead of Login.</p>
+        {error && <p>{error}</p>}
+        <input placeholder= 'Username' 
+        value = {username}
+        onChange={e => setUserName(e.target.username)} />
+
+        <input type='password' 
+        placeholder ='Password'
+        value = {Password}
+        onChange={e => setPassWord(e.target.Password)}/>
+
+        <button>Log In</button>
+        <Link to='/create-account'>Create account here</Link>
         </>
     );
 }
