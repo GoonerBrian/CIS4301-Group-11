9503954b51399a867068979d8a58360b1c5ef3d4
mralderman <92962816+mralderman@users.noreply.github.com>
2023-03-19 08:35:42 -0400

    added create account page (#5)
    * added create account page
    
    * added places for login info and link to create account

---
+++ frontend/src/pages/CreateAccountPage.js
@@ -0,0 +1,10 @@
+const CreatAccountPage = () => {
+    return (
+        <>
+        <h1>Creat Account</h1>
+        <p>Probably want to just include a button from the login page to here. Doesn't need to be linked to on other pages.</p>
+        </>
+    );
+}
+
+export default CreatAccountPage;
