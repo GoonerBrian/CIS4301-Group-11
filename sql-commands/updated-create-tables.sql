5ad4fc4baa1a3df31238668f41d77426522ab003
GoonerBrian <brian.hoblin@gmail.com>
2023-03-22 21:29:32 -0400

    Updated base on review

--- sql-commands/updated-create-tables.sql
+++ sql-commands/updated-create-tables.sql
@@ -5,23 +5,20 @@ create table crop_data(
     item_name varchar2(70),
     element_code number not null,
     element_name varchar2(70),
-    year_code number not null,
-    year number,
+    year number not null,
     unit varchar2(20),
     value decimal(20,7),
-    constraint crop_data_pk primary key (area_code, item_code, element_code, year_code)
+    constraint crop_data_pk primary key (area_code, item_code, element_code, year)
 );
 
 create table pop_data(
-    location_id number not null,
-    location_name varchar2(100),
-    variant_id number,
-    variant_type varchar2(20),
-    time_year number not null,
-    year_mid decimal(5,2),
+    location_name varchar2(100) not null,
+    year number not null,
     pop_male decimal(20,4),
     pop_female decimal(20,4),
     pop_total decimal(20,4),
     pop_density decimal(20,4),
-    constraint pop_data_pk primary key (location_id, time_year)
+    constraint pop_data_pk primary key (location_name, time_year)
 );
+
+drop table pop_data
