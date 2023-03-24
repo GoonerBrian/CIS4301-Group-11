ada6f83898d4955973d731c9a424bf435cd549ec
mralderman <92962816+mralderman@users.noreply.github.com>
2023-03-23 21:04:52 -0400

    updated sql tables to include user names and queries (#36)

--- sql-commands/updated-create-tables.sql
+++ sql-commands/updated-create-tables.sql
@@ -18,5 +18,18 @@ create table pop_data(
     pop_female decimal(20,4),
     pop_total decimal(20,4),
     pop_density decimal(20,4),
-    constraint pop_data_pk primary key (location_name, time_year)
+    constraint pop_data_pk primary key (location_name, year)
+);
+
+create table User_Info(
+    user_name VARCHAR2(100),
+    pass_word VARCHAR2(100),
+    CONSTRAINT user_pk PRIMARY KEY (user_name)
+);
+
+create table User_Query(
+    user_name NOT NULL REFERENCES User_Info (user_name),
+    query_name VARCHAR2(50) NOT NULL UNIQUE,
+    query_params VARCHAR2(100),
+    CONSTRAINT user_query_pk PRIMARY KEY (user_name, query_name)
 );
