0ccde2cd0befade5e5447d8ed79c21a066c98b25
GoonerBrian <brian.hoblin@gmail.com>
2023-04-10 23:02:47 -0400

    Query inputs hard coded in frontend

--- frontend/src/pages/query3.js
+++ frontend/src/pages/query3.js
@@ -6,9 +6,26 @@ const Query3 = () => {
     // Gets whatever is following http://localhost:3000/queries-page/
     const queryId = 'query3';
 
+    const year1 = '1990';
+    const year2 = '1999';
+    const area1 = 'United States of America';
+    const area2 = 'Europe';
+    const itemName = 'Apples';
+
+    // Works       : http://localhost:5000/query3?area1=United+States+of+America&area2=Europe&year1=1990&year2=1999&itemName=Apples
+    // Doesn't work: http://localhost:5000/query3?area1=United+States+of+America&area2=Europe&year1=1990&year2=1999&itemName=Apples
+
     useEffect(() => {
         const loadQueryInfo = async () => {
-            const response = await axios.get(`http://localhost:5000/${queryId}`);
+            const response = await axios.get(
+                `http://localhost:5000/${queryId}`, 
+                {params: {
+                    area1,
+                    area2,
+                    year1,
+                    year2,
+                    itemName
+                }});
             const newQueryInfo = response.data.rows.at(0).at(0);
             setQueryInfo([newQueryInfo]);
         }
