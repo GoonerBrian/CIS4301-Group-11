80551967223f199f2ca0afd1190ca4a876a4c421
GoonerBrian <brian.hoblin@gmail.com>
2023-04-11 20:02:40 -0400

    Completed user input form

--- frontend/src/pages/query3.js
+++ frontend/src/pages/query3.js
@@ -1,39 +1,71 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import axios from 'axios';
 
 const Query3 = () => {
     const [queryInfo, setQueryInfo] = useState([]);
-    // Gets whatever is following http://localhost:3000/queries-page/
+    const [year1, setYear1] = useState("");
+    const [year2, setYear2] = useState("");
+    const [area1, setArea1] = useState("");
+    const [area2, setArea2] = useState("");
+    const [itemName, setItemName] = useState("");
     const queryId = 'query3';
 
-    const year1 = '1990';
-    const year2 = '1999';
-    const area1 = 'United States of America';
-    const area2 = 'Europe';
-    const itemName = 'Apples';
-
-    // Works       : http://localhost:5000/query3?area1=United+States+of+America&area2=Europe&year1=1990&year2=1999&itemName=Apples
-    // Doesn't work: http://localhost:5000/query3?area1=United+States+of+America&area2=Europe&year1=1990&year2=1999&itemName=Apples
-
-    useEffect(() => {
-        const loadQueryInfo = async () => {
-            const response = await axios.get(
-                `http://localhost:5000/${queryId}`, 
-                {params: {
-                    area1,
-                    area2,
-                    year1,
-                    year2,
-                    itemName
-                }});
-            const newQueryInfo = response.data.rows.at(0).at(0);
-            setQueryInfo([newQueryInfo]);
-        }
-        loadQueryInfo();
-    }, [queryId]);
+    const runQuery = async () => {
+        const response = await axios.get(
+            `http://localhost:5000/${queryId}`, 
+            {params: {
+                area1,
+                area2,
+                year1,
+                year2,
+                itemName
+            }});
+        const newQueryInfo = response.data.rows.at(0).at(1);
+        setQueryInfo([newQueryInfo]);
+    }
 
     return (
         <>
+        <div id="query-input-form">
+            <h3>Test</h3>
+            <label>
+                First Area Name: 
+                <input
+                    value={area1}
+                    onChange={e => setArea1(e.target.value)} 
+                    type="text" 
+                />
+            </label>
+            <label>
+                Second Area Name: 
+                <input 
+                    value={area2}
+                    onChange={e => setArea2(e.target.value)}
+                    type="text" />
+            </label>
+            <label>
+                Starting Year: 
+                <input 
+                    value={year1}
+                    onChange={e => setYear1(e.target.value)}
+                    type="text" />
+            </label>
+            <label>
+                Ending Year: 
+                <input 
+                    value={year2}
+                    onChange={e => setYear2(e.target.value)}
+                    type="text" />
+            </label>
+            <label>
+                Crop Name:
+                <input 
+                    value={itemName}
+                    onChange={e => setItemName(e.target.value)}
+                    type="text" />
+            </label>
+            <button onClick={runQuery}>Run Query</button>
+        </div>
         <p>This is the query result (Query 3): {queryInfo} </p>
         </>
     );
