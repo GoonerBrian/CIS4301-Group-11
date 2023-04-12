7c21ff8134776acdd1665f108b74ce40b3209a11
GoonerBrian <brian.hoblin@gmail.com>
2023-04-12 19:52:00 -0400

    Completed plot for query 3

--- frontend/src/pages/query3.js
+++ frontend/src/pages/query3.js
@@ -8,6 +8,7 @@ const Query3 = () => {
     const [cropYears, setCropYears] = useState([]);
     const [area1Values, setArea1Values] = useState([]);
     const [area2Values, setArea2Values] = useState([]);
+    const [queryYears, setQueryYears] = useState([]);
     const [year1, setYear1] = useState("");
     const [year2, setYear2] = useState("");
     const [area1, setArea1] = useState("");
@@ -55,9 +56,14 @@ const Query3 = () => {
         setArea1Values(area1Vals);
 
         const area2Vals = response.data.rows.map(row => {
-            return row.at(2);
+            return row.at(5);
         });
         setArea2Values(area2Vals);
+
+        const queryYrs = response.data.rows.map(row => {
+            return row.at(0);
+        })
+        setQueryYears(queryYrs);
     }
 
     return (
@@ -136,21 +142,31 @@ const Query3 = () => {
         <Plot 
             data={[
                 {
-                    x: cropYears,
+                    x: queryYears,
+                    xaxis: "Years",
                     y: area1Values,
+                    yaxis: "tonnes/hectares",
+                    name: area1,
                     type: 'scatter',
                     mode: 'lines+markers',
                     marker: {color: 'orange'}
                 },
                 {
-                    x: cropYears,
+                    x: queryYears,
+                    xaxis: "Years",
                     y: area2Values,
+                    yaxis: "tonnes/hectares",
+                    name: area2,
                     type: 'scatter',
                     mode: 'lines+markers',
                     marker: {color: 'blue'}
                 }
             ]}
-            layout={ {width: 640, height: 480, title: 'Test Table'} }
+            layout={ {
+                        width: 960, 
+                        height: 720, 
+                        title: `The tonnes/hectares of ${itemName} for ${area1} and ${area2} between ${queryYears[0]} and ${queryYears[queryYears.length - 1]}`
+                    } }
         />
         </>
     );
