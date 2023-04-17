427576e5e42dc6f4c3a9f3d7f6e9b0de80d7ceb7
GoonerBrian <brian.hoblin@gmail.com>
2023-04-17 18:20:29 -0400

    Added data set info

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -23,17 +23,13 @@ const HomePage = () => {
         <p>Click on the Queries page if you would like to look up data about countries, their products, and their populations!
             Click the Tuples button to see how many tuples are in the database!
         </p>
-        <h1>About our team</h1>
+        <h1>About our data set</h1>
         <p>
-            <h3>Brian Hoblin</h3>
-            <p>
-                Brian is a Software Technician at Lockheed Martin in his final semester at the University of Florida. Post-graduation he
-                will be working as a Software Engineer at Lockheed Martin working on a DevSecOps team. Brian created the documentation 
-                for this project and created the initial skeleton of the web-app to test connecting to the CISE Oracle DB. He also 
-                completed the API endpoints and UI for the <i>Sanity Check</i> query and <i>crop yield per heactare for 2 countries over 
-                a time interval</i> query.
-            </p>
+            Our data set was found on <a href='https://www.kaggle.com/datasets/raghavramasamy/crop-statistics-fao-all-countries'>kaggle.com </a> 
+            and it consists of agricultural and population data for all "countries of the world and geographical aggregates according to the 
+            United Nations M-49 list". The data set spans from 1962 to 2018 and is formatted annually.
         </p>
+        <br></br>
         <button onClick={() => {
             displayTups();
         }}
