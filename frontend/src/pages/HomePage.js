58dbe264587d0c99650f7b63d8454a26bbb0cd8a
GoonerBrian <brian.hoblin@gmail.com>
2023-04-17 17:55:59 -0400

    Update home page and added bio

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -19,10 +19,20 @@ const HomePage = () => {
 
     return (
         <>
-        <h1>Welcome to the homepage for Group 11 (This will be the the "About Us" area too)</h1>
+        <h1>Welcome to the homepage for Group 11</h1>
         <p>Click on the Queries page if you would like to look up data about countries, their products, and their populations!
-            Click on the My Profile page to see the quieris you have made!
             Click the Tuples button to see how many tuples are in the database!
+        </p>
+        <h1>About our team</h1>
+        <p>
+            <h3>Brian Hoblin</h3>
+            <p>
+                Brian is a Software Technician at Lockheed Martin in his final semester at the University of Florida. Post-graduation he
+                will be working as a Software Engineer at Lockheed Martin working on a DevSecOps team. Brian created the documentation 
+                for this project and created the initial skeleton of the web-app to test connecting to the CISE Oracle DB. He also 
+                completed the API endpoints and UI for the <i>Sanity Check</i> query and <i>crop yield per heactare for 2 countries over 
+                a time interval</i> query.
+            </p>
         </p>
         <button onClick={() => {
             displayTups();
